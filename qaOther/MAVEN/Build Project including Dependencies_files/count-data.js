a5425f322ff7a915b717d05451c8a006a6e78603
WonaKak Win81 <wonakak@gmail.com>
2016-12-25 14:33:12 +0300

    01 first docs loaded

---
+++ qaOther/MAVEN/Build Project including Dependencies_files/count-data.js
@@ -0,0 +1,5 @@
+var DISQUSWIDGETS;
+
+if (typeof DISQUSWIDGETS != 'undefined') {
+    DISQUSWIDGETS.displayCount({"text":{"and":"and","comments":{"zero":"0 Comments","multiple":"{num} Comments","one":"1 Comment"}},"counts":[{"id":"6502 http:\/\/crunchify.com\/?p=6502","comments":10}]});
+}
